447140dacb26cf59f38154e01e6688e70b49eef6
Mizae Oliveira <167149632+Leazim3@users.noreply.github.com>
2025-05-05 00:05:31 -0300

    alterei pagina inicial colocando cabecario e caixa de digitação e algumas fontes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,8 +6,10 @@ import Logo from './Logo.jsx';
 function Home() {
   return (
     <div className="home-container">
+      <header> Fome de novidade? Achamos o lugar certo pra você. </header>
       <Logo />
-      <h1>O melhor restaurante para você:</h1>
+      <h1>O melhor restaurante para você:</h1> 
+      <input type="text" />  
       <Button>Enviar</Button>
     </div>
   );
